a50aad88f5b307ac574f11450eb3362e1178b27f
John Carlo Lipa <johncarlolipa@gmail.com>
2024-01-24 22:00:51 +0800

    set up newsletter form

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -1,8 +1,23 @@
-
 export default function Newsletter() {
   return (
-    <div>
-
-    </div>
-  )
+    <form>
+      <h4>Our Newsletter</h4>
+      {/* name */}
+      <div>
+        <label htmlFor="name">Name: </label>
+        <input type="text" id="name" name="name" defaultValue="Juan" required />
+      </div>
+      {/* last name */}
+      <div>
+        <label htmlFor="lastname">Last Name: </label>
+        <input type="text" id="lastname" name="lastname" defaultValue="Dela Cruz" required />
+      </div>
+      {/* email address */}
+      <div>
+        <label htmlFor="email">Email Address: </label>
+        <input type="text" id="email" name="email" defaultValue="juandelacruz@email.com" required />
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+  );
 }
