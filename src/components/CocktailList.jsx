6d2ae100cfbf29e79b6d40c406abd85a2cc50d41
John Carlo Lipa <johncarlolipa@gmail.com>
2024-01-26 14:31:54 +0800

    change layout grid og cocktail list

--- src/components/CocktailList.jsx
+++ src/components/CocktailList.jsx
@@ -20,7 +20,7 @@ export default function CocktailList({ drinks }) {
   });
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+    <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
       {formattedDrinks.map((item) => (
         <CocktailCard key={item.id} {...item} />
       ))}
