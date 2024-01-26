4b23698ec66ce9e860732376aeb8b4b670c1db02
John Carlo Lipa <johncarlolipa@gmail.com>
2024-01-26 10:17:43 +0800

    added app logo on navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ export default function Navbar() {
   return (
     <nav className="bg-yellow-600 p-4 flex justify-between items-center">
       <Link to="/" className="text-lg font-semibold">
-        <img src="../assets/glass-empty-wine-svgrepo-com.svg" />
+        <img src="/public/glass-empty-wine-svgrepo-com.svg" className="h-8 w-10 ml-10" alt="glass" />
       </Link>
     </nav>
   );
